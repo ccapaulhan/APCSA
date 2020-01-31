9177c80ae38d967e233ede30a4094aa7b98589ba
ccapaulhan <42499860+ccapaulhan@users.noreply.github.com>
2020-01-31 10:31:51 -0800

    refresh

--- APCSA2020/src/Unit0/AsciiBox.java
+++ APCSA2020/src/Unit0/AsciiBox.java
@@ -7,31 +7,31 @@
 //Lab  -
 package Unit0;
 
-public class AsciiBox
-{
-	public static void main(String[] args)
-	{
-		System.out.println("Paul \t  1/30/20 \n\n" );
-		System.out.println("+++++++++++++++++++++++++ " );
-		System.out.println("+++++++++++++++++++++++++ " );
-		System.out.println("+++++++++++++++++++++++++ " );
-		System.out.println("+++++++++++++++++++++++++ " );
-		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
-		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
-		System.out.println("+++++++++++++++++++++++++ " );
-		System.out.println("+++++++++++++++++++++++++ " );
-		System.out.println("+++++++++++++++++++++++++ " );
-		System.out.println("+++++++++++++++++++++++++ " );
-		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
-		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );	System.out.println("+++++++++++++++++++++++++ " );
-		System.out.println("+++++++++++++++++++++++++ " );
-		System.out.println("+++++++++++++++++++++++++ " );
-		System.out.println("+++++++++++++++++++++++++ " );
-		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
-		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
-		System.out.println("+++++++++++++++++++++++++ " );
-		System.out.println("+++++++++++++++++++++++++ " );
-		System.out.println("+++++++++++++++++++++++++ " );
+public class AsciiBox {
+	public static void main(String[] args) {
+		System.out.println("Paul \t  1/30/20 \n\n");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+	
 
 	}
 }
