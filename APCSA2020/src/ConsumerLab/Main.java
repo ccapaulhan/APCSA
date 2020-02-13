a13de55292962efd48de161df3591fb24d95b07b
ccapaulhan <42499860+ccapaulhan@users.noreply.github.com>
2020-02-13 11:07:24 -0800

    consumerLab

---
+++ APCSA2020/src/ConsumerLab/Main.java
@@ -0,0 +1,20 @@
+package ConsumerLab;
+
+import java.io.File;
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+	  
+	//test reading from files
+	//read in the positive adjectives in postiveAdjectives.txt
+	
+	System.out.println(Review.sentimentVal("crossing"));
+
+	System.out.println(Review.totalSentiment("src/ConsumerLab/simpleReview.txt"));
+	
+	System.out.println(Review.totalSentiment("src/ConsumerLab/26WestReview.txt"));
+
+
+  }
+}
